9621e6a4b3401d6efb3339d62cffac26bcb1fbaf
Vlad Sandu <sanduvlad.dev@gmail.com>
2026-06-22 22:25:51 +0300

    Implement GetUserByEmail and duplicate email check

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -2,10 +2,13 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"myprojects/internal/models"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+var ErrUserNotFound = errors.New("User not found")
 
 type UserRepository struct {
 	pool *pgxpool.Pool
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,7 +2,11 @@ package service
 
 import (
 	"errors"
+	"myprojects/internal/dto"
 	"myprojects/internal/models"
+	"myprojects/internal/repository"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var ErrUsersNotFound = errors.New("users not found")
@@ -23,10 +27,50 @@ type UserRepository interface {
 	GetUserByID(id int64) (models.User, error)
 	CreateUser(user models.User) error
 	GetAllUsers() ([]models.User, error)
+	GetUserByEmail(email string) (models.User, error)
 }
 
 func (s *UserService) GetUserByID(id int64) (models.User, error) {
 	return s.Repo.GetUserByID(id)
+}
+
+func (s *UserService) CreateUser(req dto.CreateUserRequest) error {
+	if req.Username == "" {
+		return errors.New("username is required")
+	}
+
+	if req.Email == "" {
+		return errors.New("email is required")
+	}
+
+	if len(req.Password) < 8 {
+		return errors.New("password must be at least 8 characters")
+	}
+
+	_, err := s.Repo.GetUserByEmail(req.Email)
+	if err != nil {
+		return ErrUserAlreadyExists
+	}
+
+	if !errors.Is(err, repository.ErrUserNotFound) {
+		return err
+	}
+
+	hash, err := bcrypt.GenerateFromPassword(
+		[]byte(req.Password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return err
+	}
+
+	user := models.User{
+		Username:     req.Username,
+		Email:        req.Email,
+		PasswordHash: string(hash),
+	}
+
+	return s.Repo.CreateUser(user)
 }
 
 func (s *UserService) GetAllUsers() ([]models.User, error) {
